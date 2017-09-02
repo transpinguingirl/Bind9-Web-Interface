1ecf0d66947b1ce091ddd017986f7ea80372a087
transpinguingirl <31223114+transpinguingirl@users.noreply.github.com>
2017-09-02 23:05:38 +0200

    added TTL to types Strarted working on zonefile praser

--- Deamon/bind9/parser.py
+++ Deamon/bind9/parser.py
@@ -1,2 +1,7 @@
 def loadzone(file):
-    zonefile = open(file)
+    with open(file) as zonefile:
+        for line in zonefile:
+            if "$TTL" in line:
+                TTL = line.split(" ")[1]
+            else if ""
+            
--- Deamon/bind9/types.py
+++ Deamon/bind9/types.py
@@ -1,4 +1,5 @@
 class zone:
+    TTL          = []
     SOArecords   = []
     Arecords     = []
     AAAArecords  = []
