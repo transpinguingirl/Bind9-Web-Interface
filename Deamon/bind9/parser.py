38ebf387eeecab9aa5ec7aabcd573d670d8382b2
transpinguingirl <31223114+transpinguingirl@users.noreply.github.com>
2017-09-03 22:11:55 +0200

    parser can now distinguish between A AAAA SOA NS MX CNAME DNAME TXT recods and has a default

--- Deamon/bind9/parser.py
+++ Deamon/bind9/parser.py
@@ -1,7 +1,28 @@
 def loadzone(file):
     with open(file) as zonefile:
         for line in zonefile:
-            if "$TTL" in line:
+            if "$TTL " in line:
                 TTL = line.split(" ")[1]
-            else if ""
-            
+            else if "SOA" in line:
+                print("SOA")
+            else if " NS " in line:
+                print("NS")
+            else if " A " in line:
+                print("A")
+            else if " MX " in line:
+                print("MX")
+            else if " AAAA " in line:
+                print("AAAA")
+            else if " CNAME " in line:
+                print("CNEM")
+            else if " DNAME " in line:
+                print("DNAME")
+            else if " TXT " in line:
+                print("TXT")
+            else if " IN " in line:
+                print("IN")
+def record(data):
+    data = data.split(" IN ")
+    data[0] = data[0].replace(" ","")
+    data[1] = data[1].replace(" ","")
+    return(data[0],data[1])
